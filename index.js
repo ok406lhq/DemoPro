62bc2928bd64e62ea95ccf2f9c9a3380757a9fd6
lam00 <lam009@qq.com>
2018-09-30 17:01:59 +0800

    截图

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from "./app/PicPickerDemo/App";
+import MovieListScreen from "./app/FetchDemo/SampleAppMovies";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MovieListScreen);
